c083b0cd380999f4fd39f862ee84835455ce630f
Marinus <marinus.vonhof@live.nl>
2025-01-16 14:37:03 +0100

    Initial update

---
+++ config.js
@@ -0,0 +1,35 @@
+var respecConfig =
+{
+  //specStatus: "unofficial", // w3c-UD.css nodig, maar die is er niet
+  specStatus: "design",
+  //specType: "HR",             // Handreiking
+  //format: "markdown",         // Altijd "markdown"     
+  shortName: "Beheer_Infiltratievoorzieningen", 	      // Wordt gebruikt in de document URL
+  license: "cc0",
+   
+  editors: [{
+    name: "Eric Oosterom",
+    url: "mailto:eric.oosterom@rioned.org",
+    company: "Stichting RIONED",
+    companyURL: "https://www.riool.net"
+  }],
+  authors: [{
+    name: "Wouter van Riel",
+    url: "mailto:wouter.van.riel@infralytics.org",
+    company: "infralytics",
+    companyURL: "https://www.infralytics.org"
+  }],
+  otherLinks: [{
+    key: "GitHub", href: "https://github.com/stichtingrioned/Beheer_Infiltratievoorzieningen/"
+  }],
+  //github: "https://github.com/StichtingRIONED/GWSW"
+  //-- Repositorynaam/issues op GitHub ------------------------------------------------
+  //issueBase: "https://github.com/StichtingRIONED/GWSW/issues/",
+
+  thisVersion: "https://stichtingrioned.github.io/Beheer_Infiltratievoorzieningen/",
+  latestVersion: "", // gepubliceerde versie
+  edDraftURI: "https://stichtingrioned.github.io/Beheer_Infiltratievoorzieningen/", // werkversie
+
+  maxTocLevel: 3, // Aantal niveau's ToC, default is 0/geen limiet
+  level2: true, // mv: hoofdstuk-nummering alleen voor de subsections (20210212)
+};
